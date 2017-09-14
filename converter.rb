7f4db986e3da0d726a082c733d3032e65b0cbf74
Bill <bill.rich@gmail.com>
2017-09-14 10:25:52 -0700

    Use trollop to allow max_geo and quality settings

--- converter.rb
+++ converter.rb
@@ -1,12 +1,18 @@
-#!/usr/bin/ruby
+#!/usr/bin/env ruby
 
 require 'rmagick'
+require 'trollop'
+
+opts = Trollop::options do
+  opt :max_geometry, "Maximum geometry for processes pictures (default:1920x1280)", :type => :string, :default => "1920x1280"
+  opt :quality, "Jpeg quality to save pictures at (default:70)", :type => :integer, :default => 70
+end
 
 ARGV.each do |file|
 	img = Magick::Image.read(file).first
 	img.strip!
-	img.change_geometry!("1920x1280"){ |cols, rows, img|
+	img.change_geometry!(opts[:max_geometry]){ |cols, rows, img|
 		img.resize!(cols, rows)
 	}
-	img.write(img.filename) { self.quality = 70 }
+	img.write(img.filename) { self.quality =opts[:quality] }
 end
